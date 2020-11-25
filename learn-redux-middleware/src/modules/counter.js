bd5a7ccb751e51a77de243952450c612d686134c
kororo11 <kororo11@naver.com>
2020-11-25 20:09:18 +0900

    CounterContainer 에 액션 생성 함수 변경

--- learn-redux-middleware/src/modules/counter.js
+++ learn-redux-middleware/src/modules/counter.js
@@ -7,13 +7,13 @@ export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
 
 // 1초 뒤에 increase 혹은 decrease 함수를 디스패치 함
-export const increaseAsync = () => despatch => {
+export const increaseAsync = () => dispatch => {
 	setTimeout(() => {
 		dispatch(increase());
 	}, 1000);
 };
 
-export const decreaseAsync = () => despatch => {
+export const decreaseAsync = () => dispatch => {
 	setTimeout(() => {
 		dispatch(decrease());
 	}, 1000);
