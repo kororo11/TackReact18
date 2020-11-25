9a45785965ff32b63b137e62fb8a6dd15ea3df02
kororo11 <kororo11@naver.com>
2020-11-25 19:07:14 +0900

    App 에 CounterContainer를 렌더링하기

--- learn-redux-middleware/src/App.js
+++ learn-redux-middleware/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import CounterContainer from './containers/CounterContainer';
 
 const App = () => {
 	return (
 		<div>
-			<h2>18장 리덕스 미들웨어를 통한 비동기 작업관리</h2>
+			<CounterContainer />
 		</div>
 	);
 };
