e91b8917a617fb269011b2345d7d7e58347cf17d
kororo11 <kororo11@naver.com>
2020-11-27 14:44:03 +0900

    API를 사용하여 데이터를 받아와 상태를 관리할 sample 리듀서 생성

--- learn-redux-middleware/src/modules/index.js
+++ learn-redux-middleware/src/modules/index.js
@@ -1,10 +1,10 @@
-const { combineActions } = require('redux-actions');
-
 import { combineReducers } from 'redux';
 import counter from './counter';
+import sample from './sample';
 
 const rootReducer = combineReducers({
 	counter,
+	sample,
 });
 
 export default rootReducer;
